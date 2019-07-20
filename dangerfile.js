13bd30ed075abcc85fa774b38cf779867cd48402
gabriel <ggarciaseco@gmail.com>
2019-07-20 12:43:06 +0200

    not execute tests

--- dangerfile.js
+++ dangerfile.js
@@ -4,7 +4,7 @@ const first = require("lodash.first");
 
 const hasCHANGELOGChanges = includes(danger.git.modified_files, "CHANGELOG.md")
 const hasLibraryChanges = first(danger.git.modified_files, path => path.startsWith("src/"))
-console.log("hasLibraryChanges", hasLibraryChanges);
+console.log("hasLibraryChanges", hasLibraryChanges)
 if (hasLibraryChanges && !hasCHANGELOGChanges) {
   warn("This pull request may need a CHANGELOG entry.")
 }
