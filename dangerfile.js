5210fddebb8e89e82e9968502fd165323cdd578f
gabriel <ggarciaseco@gmail.com>
2019-07-20 12:53:33 +0200

    update danger file

--- dangerfile.js
+++ dangerfile.js
@@ -4,6 +4,6 @@ const includes = require("lodash.includes");
 const hasCHANGELOGChanges = includes(danger.git.modified_files, "CHANGELOG.md")
 const hasLibraryChanges = danger.git.modified_files.filter(path => path.startsWith("src/"))
 console.log("hasLibraryChanges", hasLibraryChanges);
-if (hasLibraryChanges && !hasCHANGELOGChanges) {
-  warn("This pull request may need a CHANGELOG entry.")
+if (hasLibraryChanges.length > 0 && !hasCHANGELOGChanges) {
+  fail("This pull request may need a CHANGELOG entry.")
 }
