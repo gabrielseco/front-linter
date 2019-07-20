a2a7f3954d99079293451e78cd7b256fcede89a5
gabriel <ggarciaseco@gmail.com>
2019-07-20 12:33:31 +0200

    danger-ci task

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,9 @@
-const hasChangelog = includes(danger.git.modified_files, "CHANGELOG.md")
+import { danger, fail, warn } from "danger"
+import includes from "lodash.includes"
+import first from "lodash.first"
 
-if (!hasChangelog) {
-  warn("Please add a changelog entry for your changes.")
+const hasCHANGELOGChanges = includes(danger.git.modified_files, "CHANGELOG.md")
+const hasLibraryChanges = first(danger.git.modified_files, path => path.startsWith("src/"))
+if (hasLibraryChanges && !hasCHANGELOGChanges) {
+  warn("This pull request may need a CHANGELOG entry.")
 }
