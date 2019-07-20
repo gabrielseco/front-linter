338cacc46e278afa8e90915b554b08138a0d580f
Gabriel García Seco <ggarciaseco@gmail.com>
2019-07-20 13:01:09 +0200

    Feature/add danger (#18)
    * Add danger file to check changelog files

---
+++ dangerfile.js
@@ -0,0 +1,8 @@
+const { danger, fail, warn } = require("danger");
+const includes = require("lodash.includes");
+
+const hasCHANGELOGChanges = includes(danger.git.modified_files, "CHANGELOG.md")
+const hasLibraryChanges = danger.git.modified_files.filter(path => path.startsWith("src/"))
+if (hasLibraryChanges.length > 0 && !hasCHANGELOGChanges) {
+  fail("This pull request may need a CHANGELOG entry.")
+}
