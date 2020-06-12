911f400bd2c12f6544dafffc4a80fd078595556a
Gabriel García Seco <ggarciaseco@gmail.com>
2020-06-13 00:42:14 +0200

    Update packages and prettier (#28)
    * Update packages and prettier
    
    * update lock
    
    * Update changelog
    
    Co-authored-by: gabriel garcia <gabriel@streamloots.com>

--- typescript-react-eslint.js
+++ typescript-react-eslint.js
@@ -111,7 +111,7 @@ module.exports = {
         useTabs: false,
         parser: 'typescript',
         jsxBracketSameLine: false,
-        arrowParens: 'avoid',
+        arrowParens: 'always',
         quoteProps: 'consistent'
       }
     ]
