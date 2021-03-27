fdffd87166bfbfa4de620c8bbe0798b4d9391c2a
Gabriel García Seco <ggarciaseco@gmail.com>
2021-03-27 19:49:34 +0100

    Refactor to overrides api (#95)
    * Refactor to overrides api
    
    * Fix presets

--- scripts/createEslintFiles.js
+++ scripts/createEslintFiles.js
@@ -1,50 +1,34 @@
 const fs = require('fs');
+
 const prettier = require('prettier');
+
 const prettierOptions = require('./../.prettierrc.js');
 const CONFIGURATION = require('./../src/eslint');
 const { PRESET_TYPES } = require('./../src/presets');
 
 const presets = {
-  defaultPreset: [PRESET_TYPES.JAVASCRIPT],
-  typescriptPreset: [PRESET_TYPES.TYPESCRIPT],
-  reactJavascript: [PRESET_TYPES.JAVASCRIPT, PRESET_TYPES.REACT],
-  reactTypescript: [PRESET_TYPES.TYPESCRIPT, PRESET_TYPES.REACT]
+  defaultPreset: [PRESET_TYPES.DEFAULT],
+  reactPreset: [PRESET_TYPES.REACT]
 };
 
-const MAKE_CONFIG_FILE = config =>
+const MAKE_CONFIG_FILE = (config) =>
   prettier.format(
     `module.exports = ${JSON.stringify(config, null, 2)}`,
     prettierOptions
   );
 
 fs.writeFile(
-  './default-preset-eslint.js',
+  './eslint.js',
   MAKE_CONFIG_FILE(CONFIGURATION(presets.defaultPreset)),
-  err => {
+  (err) => {
     console.log('error default', err);
   }
 );
 
 fs.writeFile(
-  './typescript-preset-eslint.js',
-  MAKE_CONFIG_FILE(CONFIGURATION(presets.typescriptPreset)),
-  err => {
-    console.log('error typescript default', err);
-  }
-);
-
-fs.writeFile(
-  './javascript-react-eslint.js',
-  MAKE_CONFIG_FILE(CONFIGURATION(presets.reactJavascript)),
-  err => {
-    console.log('error javascript-react', err);
-  }
-);
-
-fs.writeFile(
-  './typescript-react-eslint.js',
-  MAKE_CONFIG_FILE(CONFIGURATION(presets.reactTypescript)),
-  err => {
-    console.log('error typescript-react', err);
+  './eslint-react.js',
+  MAKE_CONFIG_FILE(CONFIGURATION(presets.reactPreset)),
+  (err) => {
+    console.log('error react default', err);
   }
 );
